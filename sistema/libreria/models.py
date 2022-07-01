26e424af496dd97c9313c04007d754f9e2536ea8
haroldadrada <andresadrada@gmail.com>
2022-06-30 20:00:46 -0500

    Conexión a Base de Datos, creación del primero módelo y hacer las migraciones. Configuración de Admin

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
+    descripcion = models.TextField(verbose_name='Descripción', null=True)
