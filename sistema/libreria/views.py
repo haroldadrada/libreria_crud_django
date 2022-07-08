6934f2d324f51ede84754fb408b5081a377c6c86
haroldadrada <andresadrada@gmail.com>
2022-07-07 20:48:37 -0500

    Creación de la estructura del formulario(forms.py)

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Libro
+from .forms import LibroForm
 # Create your views here.
 
 def inicio(request):
@@ -14,7 +15,8 @@ def libros(request):
     return render(request, 'libreria/index.html', {'libros': libros})
 
 def crear(request):
-    return render(request, 'libreria/crear.html')
+    formulario = LibroForm(request.POST or None)
+    return render(request, 'libreria/crear.html', {'formulario': formulario})
 
 def editar(request):
     return render(request, 'libreria/editar.html')
